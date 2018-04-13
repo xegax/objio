b93213ea66dae6a7275a8440187eda127c1a604f
xega <xxegax@gmail.com>
2018-04-14 01:11:41 +0300

    missing publisher.ts

---
+++ src/common/publisher.ts
@@ -0,0 +1,23 @@
+import * as objio from 'objio';
+
+export class Publisher implements objio.Publisher {
+  private observers = Array<() => void>();
+
+  subscribe(o: () => void) {
+    this.observers.indexOf(o) == -1 && this.observers.push(o);
+  }
+
+  unsubscribe(o: () => void) {
+    this.observers.splice(this.observers.indexOf(o), 1);
+  }
+
+  notify() {
+    this.observers.forEach(notify => {
+      try {
+        notify();
+      } catch (e) {
+        console.log(e);
+      }
+    });
+  }
+}
