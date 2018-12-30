4184ea0522fff172c6d1ed84a522176cee1774f4
xega <xxegax@gmail.com>
2018-12-30 23:59:40 +0300

    fix calculate statistics of numberic values

--- src/common/reader/statistics.ts
+++ src/common/reader/statistics.ts
@@ -13,17 +13,26 @@ export interface StatItem {
 }
 
 export type StatMap = {[key: string]: StatItem};
+export interface  PushStatArgs {
+  objs: Array<Object>;
+  statMap: StatMap;
+  emptyStrIsNull?: boolean;
+}
 
-export function pushStat(objs: Array<Object>, statMap: StatMap) {
-  for (let n = 0; n < objs.length; n++) {
-    let obj = objs[n];
+export function pushStat(args: PushStatArgs) {
+  for (let n = 0; n < args.objs.length; n++) {
+    let obj = args.objs[n];
     Object.keys(obj).forEach(key => {
-      let item = statMap[key] || ( statMap[key] = { nulls: 0, nums: 0, bools: 0, strs: 0, ints: 0 });
+      let item = args.statMap[key] || ( args.statMap[key] = { nulls: 0, nums: 0, bools: 0, strs: 0, ints: 0 });
       const value = obj[key];
       if (value == null) {
         item.nulls++;
       } else if (typeof value == 'string') {
-        item.strs++;
+        if (args.emptyStrIsNull && value.length == 0)
+          item.nulls++;
+        else
+          item.strs++;
+
         if (item.minSize == null)
           item.minSize = value.length;
         else
@@ -62,7 +71,7 @@ export function pushStat(objs: Array<Object>, statMap: StatMap) {
         if (item.maxValue == null)
           item.maxValue = value;
         else
-          item.maxValue = Math.min(item.maxValue, value);
+          item.maxValue = Math.max(item.maxValue, value);
       }
     });
   }
